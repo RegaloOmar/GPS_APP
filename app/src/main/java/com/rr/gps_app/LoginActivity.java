573660b1b5a2d88dc517cb932edefdf633338da5
Omar Regalado <regaladomendozaomar@gmail.com>
2019-06-01 08:33:01 -0500

    Intento de llegar a XAMPP Hee Hee

--- app/src/main/java/com/rr/gps_app/LoginActivity.java
+++ app/src/main/java/com/rr/gps_app/LoginActivity.java
@@ -6,7 +6,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.rr.gps_app.MainActivity;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -28,13 +40,44 @@ public class LoginActivity extends AppCompatActivity {
         btn_InicoSesion.setOnClickListener(new View.OnClickListener() {
                                                @Override
                                                public void onClick(View v) {
-                                                   Intent i;
-                                                   i =  new Intent(LoginActivity.this, MainActivity.class);
-                                                   startActivity(i);
+
+                                                   ejecutarServicio("http://192.168.64.2:8080/Developer/Insertar_usuario.php");
+
+                                                   //Intent i;
+                                                   //i =  new Intent(LoginActivity.this, MainActivity.class);
+                                                   // startActivity(i);
                                                }
                                            }
 
         );
 
+    }
+
+    //Conexion al servidor XAMPP
+    private void ejecutarServicio(String URL){
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Toast.makeText(getApplicationContext(), "Operación Exitosa", Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "Algo salio mal", Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> parametros = new HashMap<String, String>();
+                //Datos a insertar primero va el nombre declarado en PHP
+                //despues el campo del formulario donde sacaremos la info ("PHP", formulario)
+                parametros.put("usuario", email_text.getText().toString());
+                parametros.put("pasword", pass_text.getText().toString());
+                return super.getParams();
+            }
+        };
+        //Procesar las peticiones hechas por la app para que la libreria Volley las ejecute
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 }
