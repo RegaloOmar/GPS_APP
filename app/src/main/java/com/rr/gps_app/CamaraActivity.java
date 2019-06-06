7c07f57df866ca807cf03703814b3ca5aee403e8
Omar Regalado <regaladomendozaomar@gmail.com>
2019-06-06 00:23:02 -0500

    intento de subir foto hee hee

--- app/src/main/java/com/rr/gps_app/CamaraActivity.java
+++ app/src/main/java/com/rr/gps_app/CamaraActivity.java
@@ -7,6 +7,7 @@ import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,14 +15,27 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
  public class CamaraActivity extends AppCompatActivity {
 
-    private Button btn_Camara;
+    private Button btn_Camara, btn_CargarFotos;
     private ImageView foto1, foto2, foto3, foto4;
     final static int cons = 0;
     public int contador = 0;
-    Bitmap bmp;
-     SessionManager sessionManager;
+    Bitmap bmp, bmpFoto1;
+    SessionManager sessionManager;
+    StringRequest stringRequest;
+    RequestQueue request;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +46,7 @@ import android.widget.Toast;
         initCamara();
 
         btn_Camara = (Button) findViewById(R.id.btnCamara);
+        btn_CargarFotos = (Button) findViewById(R.id.btnCargarFotos);
 
         foto1 = (ImageView) findViewById(R.id.image1);
         foto2 = (ImageView) findViewById(R.id.image2);
@@ -46,6 +61,13 @@ import android.widget.Toast;
                 Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 contador++;
                 startActivityForResult(i, cons);
+            }
+        });
+
+        btn_CargarFotos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cargarWebService();
             }
         });
         
@@ -67,6 +89,7 @@ import android.widget.Toast;
             if(contador == 1){
                 Bundle ext = data.getExtras();
                 bmp = (Bitmap)ext.get("data");
+                bmpFoto1 = bmp;
                 foto1.setImageBitmap(bmp);
             }else if(contador == 2){
                 Bundle ext = data.getExtras();
@@ -103,8 +126,52 @@ import android.widget.Toast;
          }
      }
 
-     private void logout() {
-         Intent i = new Intent(CamaraActivity.this, LoginActivity.class);
-         startActivity(i);
+     private void cargarWebService(){
+
+         String url = "https://rrdevsolutions.com/ScriptsPHP/pruebaFoto.php?";
+
+         stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+             @Override
+             public void onResponse(String response) {
+
+                 if(response.trim().equalsIgnoreCase("registra")) {
+                     Toast.makeText(CamaraActivity.this, "Se registro con exito", Toast.LENGTH_SHORT).show();
+                 } else {
+                     Toast.makeText(CamaraActivity.this, "No se registro", Toast.LENGTH_SHORT).show();
+                 }
+
+             }
+         }, new Response.ErrorListener() {
+             @Override
+             public void onErrorResponse(VolleyError error) {
+                 Toast.makeText(CamaraActivity.this, "A ocurrido un error", Toast.LENGTH_SHORT).show();
+             }
+         }){
+             @Override
+             protected Map<String, String> getParams() throws AuthFailureError {
+
+                 String imagen1 = convertirImagen1(bmp);
+                 String nombre = "Foto1";
+
+                 Map<String, String> parametros= new HashMap<>();
+                 parametros.put("nombre", nombre);
+                 parametros.put("imagen", imagen1);
+                 return super.getParams();
+             }
+         };
+         request.add(stringRequest);
      }
+
+
+     private String convertirImagen1(Bitmap bmp){
+
+         ByteArrayOutputStream array = new ByteArrayOutputStream();
+         bmp.compress(Bitmap.CompressFormat.JPEG,100,array);
+         byte[] imagenByte = array.toByteArray();
+         String imagenString = Base64.encodeToString(imagenByte, Base64.DEFAULT);
+         return  imagenString;
+
+     }
+
+
  }
