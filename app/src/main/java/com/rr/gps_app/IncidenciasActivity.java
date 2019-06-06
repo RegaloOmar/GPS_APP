b3881ccdee8ea9096d8f02a03c7ecc473deaff44
Victor <victormtze94@gmail.com>
2019-06-06 01:05:59 -0500

    incidencia

--- app/src/main/java/com/rr/gps_app/IncidenciasActivity.java
+++ app/src/main/java/com/rr/gps_app/IncidenciasActivity.java
@@ -1,5 +1,6 @@
 package com.rr.gps_app;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,14 +8,29 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class IncidenciasActivity extends AppCompatActivity {
 
 
     private Button btnIncidencia;
+    private EditText edtIncidencia;
     SessionManager sessionManager;
+    private ProgressDialog pDialog;
     String talon;
+    String URL = "https://rrdevsolutions.com/cdm/master/request/insertIncidencia.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,13 +44,17 @@ public class IncidenciasActivity extends AppCompatActivity {
 
         talon = getIntent().getStringExtra("talon");
 
+        edtIncidencia = findViewById(R.id.txtIncidencias);
         btnIncidencia = findViewById(R.id.btnMandar);
 
         Toast.makeText(getApplicationContext(),talon,Toast.LENGTH_LONG).show();
 
         btnIncidencia.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v)
+            {
+                incidencia(URL);
+                dialog();
                 Intent i = new Intent(IncidenciasActivity.this, SemaforoActivity.class);
                 startActivity(i);
             }
@@ -42,6 +62,37 @@ public class IncidenciasActivity extends AppCompatActivity {
 
 
 
+    }
+
+    private void incidencia(String URL)
+    {
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response)
+            {
+
+                //Toast.makeText(getApplicationContext(),"Se han insertado datos",Toast.LENGTH_LONG).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error)
+            {
+                Toast.makeText(getApplicationContext(),error.toString(),Toast.LENGTH_LONG).show();
+            }
+        })
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError
+            {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("talon",talon);
+                params.put("incidencia",edtIncidencia.getText().toString());
+                return params;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 
     @Override
@@ -60,6 +111,16 @@ public class IncidenciasActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    //Metodo para crear dialogos al momento de hacer cambios
+    public void dialog()
+    {
+        pDialog = new ProgressDialog(IncidenciasActivity.this);
+        pDialog.setMessage("Mandando Incidencia...");
+        pDialog.setIndeterminate(false);
+        pDialog.setCancelable(true);
+        pDialog.show();
     }
 
     private void logout() {
