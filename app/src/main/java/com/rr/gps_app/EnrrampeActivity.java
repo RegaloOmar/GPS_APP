39503cf8c3dcf923e3daee62eef75e226ffae183
Victor <victormtze94@gmail.com>
2019-06-10 21:26:39 -0500

    heehee

--- app/src/main/java/com/rr/gps_app/EnrrampeActivity.java
+++ app/src/main/java/com/rr/gps_app/EnrrampeActivity.java
@@ -1,6 +1,7 @@
 package com.rr.gps_app;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
@@ -12,15 +13,18 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
+import java.sql.Time;
 import java.util.Calendar;
 
 public class EnrrampeActivity extends AppCompatActivity {
 
     String talon;
-    private ImageButton imgDatePicker;
+    private ImageButton imgDatePicker,imgTimePicker;
+    private TimePickerDialog.OnTimeSetListener mTimeSetListener;
     private DatePickerDialog.OnDateSetListener mDateSetListener;
-    private EditText txtFechaNac;
+    private EditText txtDate,txtHour;
     private static final String TAG = "EnrrampeActivity";
 
     @Override
@@ -29,8 +33,10 @@ public class EnrrampeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_enrrampe);
 
         talon = getIntent().getStringExtra("talon");
-        txtFechaNac = findViewById(R.id.fechaNacimiento);
+        txtDate = findViewById(R.id.fechaNacimiento);
+        txtHour = findViewById(R.id.textTime);
         imgDatePicker = findViewById(R.id.imageButton1);
+        imgTimePicker = findViewById(R.id.imageButton2);
 
         //Mediante eventos mandamos a llamar a la vista del calendario
         imgDatePicker.setOnClickListener(new View.OnClickListener() {
@@ -60,10 +66,35 @@ public class EnrrampeActivity extends AppCompatActivity {
                 Log.d(TAG,"onDateSet: mm/dd/yyyy; " + month + "/" + day + "/" + year);
 
                 String date = month + "/" + day + "/" + year;
-                txtFechaNac.setText(date);
+                txtDate.setText(date);
             }
         };
 
+        //Mediante el evento convocamos al time picker
+        imgTimePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar c = Calendar.getInstance();
+                int hour = c.get(Calendar.HOUR_OF_DAY);
+                int minute = c.get(Calendar.MINUTE);
+                TimePickerDialog timePickerDialog = new TimePickerDialog(EnrrampeActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mTimeSetListener,hour,minute,android.text.format.DateFormat.is24HourFormat(getApplicationContext()));
 
+                timePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                timePickerDialog.show();
+
+            }
+        });
+
+        mTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute)
+            {
+                txtHour.setText(hourOfDay + ":" + minute);
+            }
+        };
     }
+
+
 }
