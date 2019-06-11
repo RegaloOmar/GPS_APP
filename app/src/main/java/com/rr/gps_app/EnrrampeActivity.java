70d64728e35f7a0e4cdc81182dae54b318ea9a1a
Victor <victormtze94@gmail.com>
2019-06-10 21:03:10 -0500

    heehee

--- app/src/main/java/com/rr/gps_app/EnrrampeActivity.java
+++ app/src/main/java/com/rr/gps_app/EnrrampeActivity.java
@@ -1,22 +1,27 @@
 package com.rr.gps_app;
 
 import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.util.Calendar;
 
 public class EnrrampeActivity extends AppCompatActivity {
 
-    private Button btn_FechaEnrrampe;
-    private EditText txt_FechaEnrrampe;
-    private int dia, mes, año;
     String talon;
+    private ImageButton imgDatePicker;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
+    private EditText txtFechaNac;
+    private static final String TAG = "EnrrampeActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,26 +29,41 @@ public class EnrrampeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_enrrampe);
 
         talon = getIntent().getStringExtra("talon");
-        txt_FechaEnrrampe = findViewById(R.id.txtFechaEnrrampe);
+        txtFechaNac = findViewById(R.id.fechaNacimiento);
+        imgDatePicker = findViewById(R.id.imageButton1);
 
-        btn_FechaEnrrampe = findViewById(R.id.btnFechaEnrrampe);
-
-        btn_FechaEnrrampe.setOnClickListener(new View.OnClickListener() {
+        //Mediante eventos mandamos a llamar a la vista del calendario
+        imgDatePicker.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final Calendar c = Calendar.getInstance();
-                dia = c.get(Calendar.DAY_OF_MONTH);
-                mes = c.get(Calendar.MONTH);
-                año = c.get(Calendar.YEAR);
+                Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
 
-                DatePickerDialog datePickerDialog = new DatePickerDialog(EnrrampeActivity.this, new DatePickerDialog.OnDateSetListener() {
-                    @Override
-                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                        txt_FechaEnrrampe.setText(dayOfMonth+"/"+(month+1)+"/"+year);
-                    }
-                },año,mes,dia);
-                datePickerDialog.show();
+                DatePickerDialog dialog = new DatePickerDialog(
+                        EnrrampeActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDateSetListener,
+                        year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
             }
         });
+
+        //Mediante esta variable obtenemos la fecha del calendario del datepicker
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day)
+            {
+                month = month + 1;
+                Log.d(TAG,"onDateSet: mm/dd/yyyy; " + month + "/" + day + "/" + year);
+
+                String date = month + "/" + day + "/" + year;
+                txtFechaNac.setText(date);
+            }
+        };
+
+
     }
 }
